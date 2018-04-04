4475ce631a8ecd09ad923939fd7b5cc2fbcb1108
Conner P. Theberge <thebergec@wit.edu>
2018-04-04 11:00:25 -0400

    Added some query stubs
    check to ensure these are what we need.

--- src/com/group13/queries/AdminQuery.java
+++ src/com/group13/queries/AdminQuery.java
@@ -1,7 +1,19 @@
 package com.group13.queries;
 
+import java.util.LinkedList;
+
 public class AdminQuery {
 
+	public LinkedList<Integer> getAllCreators() {
+		return new LinkedList<Integer>();
+	}
 	
+	public double getTotalCost() {
+		return 0.0;
+	}
+	
+	public double getCreatorPayment(int creatorId) {
+		return 0.0;
+	}
 	
 }
--- src/com/group13/queries/MediaQuery.java
+++ src/com/group13/queries/MediaQuery.java
@@ -1,5 +1,63 @@
 package com.group13.queries;
 
+import java.io.File;
+import java.util.LinkedList;
+
 public class MediaQuery {
 
+	public LinkedList<Integer> getAllMedia() {
+		//TODO stub
+		return new LinkedList<>();
+	}
+	
+	public LinkedList<Integer> getAllMedia(String tag) {
+		//TODO stub
+		return new LinkedList<>();
+	}
+	
+	public String getMediaTitle(int mediaID) {
+		//TODO stub
+		return "Media Title";
+	}
+	
+	public String getLastOpened(int mediaID) {
+		//TODO stub
+		return "10/31/1996";
+	}
+	
+	public String getDateCreated(int mediaID) {
+		//TODO stub
+		return "10/31/1996";
+	}
+	
+	public File getMediaFile(int mediaID) {
+		//TODO stub
+		return null;
+	}
+	
+	public File getMediaThumbnail(int mediaID) {
+		//TODO stub
+		return null;
+	}
+	
+	public int getMediaViews(int mediaID) {
+		//TODO stub
+		return 0;
+	}
+	
+	public String getDimensions(int mediaID) {
+		//TODO stub
+		return "100x100";
+	}
+	
+	public String getRuntime(int mediaID) {
+		//TODO stub
+		return "00.00";
+	}
+	
+	public String getAlbumTitle(int mediaID) {
+		//TODO stub
+		return "Album Title";
+	}
+	
 }
