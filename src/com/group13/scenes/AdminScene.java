4a345ca33c65ccb2683feba041725a7ffbc8cb84
Conner P. Theberge <thebergec@wit.edu>
2018-04-07 16:11:16 -0400

    Commit for merging

--- src/com/group13/scenes/AdminScene.java
+++ src/com/group13/scenes/AdminScene.java
@@ -1,32 +1,152 @@
 package com.group13.scenes;
 
-/**
- * This scene is where the admin will be able to view finances,
- * change/edit media or accounts
+import java.util.Scanner;
+
+import com.group13.main.MediaCenterApplication;
+import com.group13.util.Menu;
+
+/*
+ * Admin Scene
+ * @author Tyler Crosby
+ * @author Connor Theberge
  * 
- * @author Conner Theberge
+ * 
  */
+
 public class AdminScene extends AppScene {
 	
-	/*
-	 * Edit All Media
-	 * Edit Tags
-	 * View Creators
-	 * View Finances
-	 */
+	private Menu mainMenu = new Menu("Media Center");
+	
+	private Menu accountMenu = new Menu("Account Settings");
+	
+	private Menu adminMenu = new Menu("Admin Menu");
+	
+	private Menu curMenu = mainMenu;
+	private void changeMenu(Menu menu) {
+		curMenu = menu;
+	}
 	
 	public AdminScene() {
-		// TODO Auto-generated constructor stub
+
+		mainMenu.addOption("Search Media", () -> {
+			
+			//TAKE FROM USERSCENE
+			
+		});
+		
+		mainMenu.addOption("View Media (All)", () -> {
+					
+			//TAKE FROM USERSCENE	
+					
+		});
+		
+		mainMenu.addOption("View Media (Favorites)", () -> {
+			
+			//TAKE FROM USERSCENE
+			
+		});
+		
+		mainMenu.addOption("View Media (Followed Creators)", () -> {
+			
+			//TAKE FROM USERSCENE
+			
+		});
+	
+		mainMenu.addOption("Administrator Options", () -> {
+			/*
+			 * CHANGE MENU TO ADMIN SPECIFIC MENU
+			 * @author Tyler Crosby
+			 */
+			changeMenu(adminMenu);
+			
+		});
+		
+		mainMenu.addOption("Account Settings", () -> {
+			
+			changeMenu(accountMenu);
+			
+		});
+		
+		mainMenu.addOption("Exit Application", () -> {
+			
+			System.exit(0);
+			
+		});
+	
+		accountMenu.addOption("Change Username", () -> {
+			
+			//TAKE FROM USERSCENE
+			
+		});
+		
+		accountMenu.addOption("Change Password", () -> {
+					
+			//TAKE FROM USERSCENE	
+					
+		});
+	
+		accountMenu.addOption("Back", () -> {
+			
+			changeMenu(mainMenu);
+			
+		});
+	//Admin Menu
+		adminMenu.addOption("Add Media", () -> {
+			
+			
+			
+		});
+		
+		adminMenu.addOption("Delete Media", () -> {
+			
+			
+			
+		});
+		
+		adminMenu.addOption("Modify Media", () -> {
+			
+			
+			
+		});
+		adminMenu.addOption("Back", ()-> {
+			
+			changeMenu(mainMenu);
+			
+		});
+		
+		adminMenu.addOption("Change a User's account info", () -> {
+			/*
+			 * Asks what user -> lets say John Doe.
+			 * 
+			 * Returns John's account info, IF ARTIST -> financial information, statistics, account payable info.
+			 * 
+			 * Asks what admin wants to change (switch statement)
+			 */
+			System.out.println();
+			
+			
+		});
+		
+		
 	}
 
+	private boolean loop = true;
+	
 	@Override
 	public void onStart() {
-		// TODO Auto-generated method stub
+		while (loop) {
+			
+			curMenu.print();
+			curMenu.getInput(MediaCenterApplication.INPUT_SCANNER);
+			System.out.println();
+			
+		}
 	}
 
 	@Override
 	public void onExit() {
-		// TODO Auto-generated method stub
+		
+		loop = false;
 		
 	}
 
--- src/com/group13/scenes/LoginScene.java
+++ src/com/group13/scenes/LoginScene.java
@@ -1,20 +1,70 @@
 package com.group13.scenes;
 
+import java.util.Scanner;
+
+import com.group13.main.MediaCenterApplication;
+
+
 public class LoginScene extends AppScene {
 
+	private boolean validUser = false;
+	private boolean validPass = false;
+	
+	private String inputedUser;
+	private String inputedPass;
 	public LoginScene() {
-		// TODO Auto-generated constructor stub
+	
+		
+		
+		
+		
 	}
 
+	
+	private boolean loop = true;
 	@Override
 	public void onStart() {
 		// TODO Auto-generated method stub
-		
+		while(!validUser) {
+			System.out.println("Enter your username: ");
+			inputedUser = MediaCenterApplication.INPUT_SCANNER.next();
+			
+			if(database.contains(inputedUser)) { //if inputedUser is in DB
+				
+				validUser = true;
+				
+			}
+			else {
+				System.out.println("Non existant user!");
+				System.out.println();
+				System.out.println();
+				System.out.println();
+			}
+		}
+		while(!validPass) {
+			
+			System.out.println("Enter your password: ");
+			inputedPass = MediaCenterApplication.INPUT_SCANNER.next();
+			
+			if(inputedUser.contains(inputedPass)) {
+				
+				validPass = true;
+				
+			}
+			else {
+				System.out.println("Incorrect password!");
+				System.out.println();
+				System.out.println();
+				System.out.println();
+			}
+			
+		}
 	}
 
 	@Override
 	public void onExit() {
-		// TODO Auto-generated method stub
+		
+		loop = false;
 		
 	}
 	
