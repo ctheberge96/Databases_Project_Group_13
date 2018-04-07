fdc65bb90be951952903c9b5ae5c31771f09e061
crosbyt <crosbyt@WITPC0EF342.wit.private>
2018-04-07 18:16:11 -0400

    fixed login, added update variable

--- src/com/group13/scenes/LoginScene.java
+++ src/com/group13/scenes/LoginScene.java
@@ -36,6 +36,7 @@ public class LoginScene extends AppScene {
 				
 				User user = new User(inputedUser, inputedPass);
 				if(user.isValid()) {
+					loginState = true;
 					MediaCenterApplication.currentUser = user; //accepts user as valid for moving on
 					if(Creator.isCreator(user)) {
 						MediaCenterApplication.changeScene(MediaCenterApplication.CREATOR_SCENE);
